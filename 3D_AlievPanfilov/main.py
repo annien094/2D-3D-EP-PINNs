54461c045cdfdb1284823406f9d59347acf9e339
Annie Chiu <annien094@gmail.com>
2024-04-30 12:01:22 +0100

    plot loss history

--- 3D_AlievPanfilov/main.py
+++ 3D_AlievPanfilov/main.py
@@ -11,6 +11,7 @@ import deepxde as dde # version 0.11 or higher
 from generate_plots_3d import plot_3D
 import utils
 import pinn
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -23,11 +24,12 @@ if __name__ == "__main__":
     parser.add_argument('-ht', '--heter', dest='heter', required = False, action='store_true', help='Predict heterogeneity - only in 2D')    
     parser.add_argument('-p', '--plot', dest='plot', required = False, action='store_true', help='Create and save plots')
     parser.add_argument('-a', '--animation', dest='animation', required = False, action='store_true', help='Create and save 2D Animation')
+    parser.add_argument('-rg', '--regime', dest='regime', required= True, type = str, help='regime e.g. planar, spiral')
     args = parser.parse_args()
 
 ## General Params
 noise = 0 # noise factor
-test_size = 0.8 # precentage of testing data
+test_size = 0.8 # precentage of testing dat
 
 def main(args):
     
@@ -53,14 +55,14 @@ def main(args):
     ## Define Boundary Conditions
     bc = dynamics.BC_func(args.dim, geomtime)
     ## Define Initial Conditions
-    ic = dynamics.IC_func(observe_train, v_train)
+    ic_v, ic_w = dynamics.IC_func(observe_train, v_train, args.regime)
     
     ## Model observed data
     observe_v = dde.PointSetBC(observe_train, v_train, component=0)
-    input_data = [bc, ic, observe_v]
+    input_data = [bc, ic_v, ic_w, observe_v]
     if args.w_input: ## If W required as an input
         observe_w = dde.PointSetBC(observe_train, w_train, component=1)
-        input_data = [bc, ic, observe_v, observe_w]
+        input_data = [bc, ic_v, ic_w, observe_v, observe_w]
     
     ## Select relevant PDE (Dim, Heterogeneity) and define the Network
     model_pinn = pinn.PINN(dynamics, args.dim,args.heter, args.inverse)
@@ -71,6 +73,26 @@ def main(args):
     model, losshistory, train_state = model_pinn.train(out_path, params)
     model.save("model")
     
+    ## Plot total loss history 
+    loss_train = np.sum(losshistory.loss_train, axis=1)
+    loss_test = np.sum(losshistory.loss_test, axis=1)
+    plt.figure()
+    plt.semilogy(losshistory.steps, loss_train, label="Train loss", linewidth=2)
+    plt.semilogy(losshistory.steps, loss_test, label="Test loss", linewidth=2)
+    plt.title("Total loss history "+args.model_folder_name)
+    plt.xlabel("Steps")
+    plt.legend()
+    plt.savefig(out_path + "Total loss history")
+
+    # Plot individual loss term history
+    plt.figure()
+    loss_label = ['eq_v', 'eq_w', 'IC_v', 'IC_w', 'BC', 'data']
+    plt.semilogy(losshistory.steps, losshistory.loss_train, label=loss_label, linewidth=2)
+    plt.title("Individual loss history " +args.model_folder_name)
+    plt.xlabel("Steps")
+    plt.legend()
+    plt.savefig(out_path + "Individual loss history")
+
     ## Compute rMSE
     pred = model.predict(observe_test)   
     v_pred, w_pred = pred[:,0:1], pred[:,1:2]
@@ -84,16 +106,16 @@ def main(args):
     pred_all = model.predict(observe_x)
     v_pred_all, w_pred_all = pred_all[:,0:1], pred_all[:,1:2]
 #    np.savetxt("train_data.dat", np.hstack((observe_train, v_train, w_train)),header="observe_train,v_train, w_train")
-    np.savetxt("vw_pred_all.dat", np.hstack((observe_x, v_pred_all, w_pred_all)),header="observe_x, v_pred, w_pred")
+    np.savetxt(out_path+"vw_pred_all.dat", np.hstack((observe_x, v_pred_all, w_pred_all)),header="observe_x, v_pred, w_pred")
     
     ## Generate Figures
     data_list = [observe_x, observe_train, v_train, V]
-    if args.plot and args.dim == 1:
-        plot_1D(data_list,dynamics, model, args.model_folder_name)
-    elif args.plot and args.dim == 2:
-        plot_2D(data_list,dynamics, model, args.animation, args.model_folder_name)
-    elif args.plot and args.dim == 3:
-        plot_3D(data_list,dynamics, model, args.model_folder_name)
+    # if args.plot and args.dim == 1:
+    #     plot_1D(data_list,dynamics, model, args.model_folder_name)
+    # elif args.plot and args.dim == 2:
+    #     plot_2D(data_list,dynamics, model, args.animation, args.model_folder_name)
+    # elif args.plot and args.dim == 3:
+    plot_3D(data_list,dynamics, model, out_path)
     return model
 
 ## Run main code
